067e6c05042beeae7d62bd0580d2a99788e35495
YoungwanCho <developer119korea@gmail.com>
2021-02-15 04:31:08 +0900

    이미지를 첨부하여 트윗할 수 있다

--- src/compoenents/App.js
+++ src/compoenents/App.js
@@ -5,7 +5,7 @@ import { authService } from "../fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [userObj, setUserObj] = useState(null);
-  
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
       {init ? (<AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />) : ("Initializing...")}
-      <footer>&copy;{new Date().getFullYear()} Nwitter </footer>
+      {/* <footer>&copy;{new Date().getFullYear()} Nwitter </footer> */}
     </>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { dbService } from "fbase";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
 import Nweet from "compoenents/Nweet";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     dbService.collection("nweets").onSnapshot((snapshot) => {
@@ -14,16 +16,27 @@ const Home = ({ userObj }) => {
       }));
       setNweets(nweetArray);
     });
-    }, []);
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("nweets").add({
+    let attachmentUrl = "";
+    if (attachment != null) {
+      const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+      const response = await fileRef.putString(attachment, "data_url");
+      attachmentUrl = await response.ref.getDownloadURL();
+    }
+
+    const nweetObj = {
       text: nweet,
       createdAt: Date.now(),
       creatorId: userObj.uid,
-    });
+      attachmentUrl,
+    }
+
+    await dbService.collection("nweets").add(nweetObj);
     setNweet("");
+    setAttachment(null);
   };
 
   const onChange = (event) => {
@@ -33,16 +46,39 @@ const Home = ({ userObj }) => {
     setNweet(value);
   };
 
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  }
+
+  const onClearAttachment = () => setAttachment(null);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}>
-        </input>
+        <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}></input>
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet"></input>
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
-          <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>
+          <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
         ))}
       </div>
     </div>
