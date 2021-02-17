f465421f5efef49f1e4f832688718367f24a4e6f
YoungwanCho <developer119korea@gmail.com>
2021-02-17 16:42:34 +0900

    사용자의 닉네임을 수정 할 수 있다

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { authService } from "fbase";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj, refreshUser }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const onLogoutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
+
   return (
     <>
-      <button onClick={onLogoutClick}>Log Out</button>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type="text" placeholder="Display Name" value={newDisplayName} />
+        <input type="submit" value="Update Profile" />
+      </form>
+      <button onClick={onLogoutClick}>Log out</button>
     </>
   );
 };
